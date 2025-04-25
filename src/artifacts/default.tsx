7af4e36668e6ade5834daf8bd7140012bc193892
UFO-101 <josephmiller101@gmail.com>
2025-04-25 14:58:29 +0100

    Update placeholder to real YouTube video.

--- src/artifacts/default.tsx
+++ src/artifacts/default.tsx
@@ -193,7 +193,7 @@ export default function PauseConLanding() {
           <iframe
             width="100%"
             height="100%"
-            src="https://www.youtube.com/embed/NpEaa2P7qZI"
+            src="https://www.youtube.com/embed/kWUZw1TFjbQ"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
